3b4ef583e1ebc107c8c6afb6ab3e76a0ac791800
Caio Torresi <caioct2311@gmail.com>
2019-05-06 20:05:03 -0300

    adjust payment model and add tests

--- api/models.py
+++ api/models.py
@@ -31,21 +31,19 @@ class Loan(models.Model):
 
 
 class Payment(models.Model):
-    """
-    Payment model
-    """
 
     MADE = "made"
     MISSED = "missed"
     PAYMENTS = ((MADE, "made"), (MISSED, "missed"))
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     loan = models.ForeignKey(
-        to="api.Loan", verbose_name=_("loan"), on_delete=models.DO_NOTHING
+        to="api.Loan", verbose_name=_("loan"), on_delete=models.CASCADE
     )
     payment = models.CharField(
-        verbose_name=_("payment"), max_length=20, choices=PAYMENTS, default=MISSED
+        verbose_name=_("payment"), max_length=6, choices=PAYMENTS, default=MISSED
     )
     date = models.DateTimeField(verbose_name=_("date"))
     amount = models.FloatField(verbose_name=_("amount"))
 
-    def __str__(self):
-        return self.loan
+    def __str__(self) -> str:
+        return str(self.id)
---
+++ api/tests/test_models.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from api.models import Payment, Loan
+
+
+class TestPaymentModel(TestCase):
+    def setUp(self) -> None:
+        self.loan = Loan.objects.create(
+            amount=1000, term=12, rate=0.05, date="2019-05-09 03:18Z"
+        )
+        self.payment = Payment.objects.create(
+            loan=self.loan, payment="made", date="2019-06-09 03:18Z", amount=200.00
+        )
+
+    def test_payment__str__(self):
+        self.assertEqual(str(self.payment), str(self.payment.id))
+
+    def test_loadn__str__(self):
+        self.assertEqual(str(self.loan), str(self.loan.id))
