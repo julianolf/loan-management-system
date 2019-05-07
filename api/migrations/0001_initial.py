285e19b03b797ef999a7a4a609b4452d54a7ca00
Caio Torresi <caioct2311@gmail.com>
2019-05-06 21:31:15 -0300

    modify migration files to work with Postgres

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -35,11 +35,11 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.AutoField(
-                        auto_created=True,
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
                         primary_key=True,
                         serialize=False,
-                        verbose_name="ID",
                     ),
                 ),
                 (
--- api/migrations/0002_auto_20190506_1945.py
+++ api/migrations/0002_auto_20190506_1945.py
@@ -12,13 +12,6 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AlterField(
             model_name="payment",
-            name="id",
-            field=models.UUIDField(
-                default=uuid.uuid4, editable=False, primary_key=True, serialize=False
-            ),
-        ),
-        migrations.AlterField(
-            model_name="payment",
             name="loan",
             field=models.ForeignKey(
                 on_delete=django.db.models.deletion.CASCADE,
