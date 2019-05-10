35aed6b91449d7d474d6ca80b0b7062a420ba0cd
Caio Torresi <caioct2311@gmail.com>
2019-05-10 17:24:18 -0300

    adjust loan serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,12 +3,9 @@ from rest_framework import serializers
 
 
 class LoanSerializer(serializers.ModelSerializer):
-    """
-    Serializer for Loan model
-    """
 
     id = serializers.UUIDField(write_only=True, required=False, format="hex")
-    amount = serializers.IntegerField(write_only=True)
+    amount = serializers.FloatField(write_only=True)
     term = serializers.IntegerField(write_only=True)
     rate = serializers.FloatField(write_only=True)
     date = serializers.DateTimeField(write_only=True)
@@ -16,20 +13,10 @@ class LoanSerializer(serializers.ModelSerializer):
     installment = serializers.SerializerMethodField()
     loan_id = serializers.SerializerMethodField()
 
-    def get_installment(self, loan: Loan):
-        """
-        Get installment value from model
-        :param loan:
-        :return: round(float,2)
-        """
-        return round(loan.installment, 2)
+    def get_installment(self, loan: Loan) -> float:
+        return loan.installment
 
-    def get_loan_id(self, loan: Loan):
-        """
-        Rename field from id to loan_id (computed)
-        :param loan:
-        :return: str
-        """
+    def get_loan_id(self, loan: Loan) -> str:
         return str(loan.id)
 
     class Meta:
