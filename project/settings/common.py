174788189da0bdf05d66ff21040b38a08cc3a3c1
William Galleti <william.galleti@gmail.com>
2019-05-14 16:56:32 -0400

    Allow cors (#68)

--- project/settings/common.py
+++ project/settings/common.py
@@ -9,12 +9,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "drf_yasg",
     "api",
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
